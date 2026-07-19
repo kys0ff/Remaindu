183658e23edb16eb70f8771a04bbf2880c896e71
kys0ff <idk683120@gmail.com>
2026-07-19 22:50:04 +0100

    refactor: enhance UI/UX with animations, transitions, and theme updates

---
+++ app/src/main/java/off/kys/remaindu/util/AnimationUtils.kt
@@ -0,0 +1,43 @@
+package off.kys.remaindu.util
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+
+enum class ButtonState { Pressed, Idle }
+
+fun Modifier.bounceClick() = composed {
+    var buttonState by remember { mutableStateOf(ButtonState.Idle) }
+    val scale by animateFloatAsState(
+        if (buttonState == ButtonState.Pressed) 0.95f else 1f,
+        label = "bounceScale"
+    )
+
+    this
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .pointerInput(buttonState) {
+            awaitPointerEventScope {
+                buttonState = if (buttonState == ButtonState.Pressed) {
+                    waitForUpOrCancellation()
+                    ButtonState.Idle
+                } else {
+                    awaitFirstDown(false)
+                    ButtonState.Pressed
+                }
+            }
+        }
+}
+
+const val SmoothDuration = 400
+const val FastDuration = 200
